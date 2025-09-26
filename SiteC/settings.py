95f400004ae124a48c2b8a03c712aa1792fd2338
zenfore1 <naxim2006@gmail.com>
2025-09-26 21:06:21 +0300

    аутентификация в drf

--- SiteC/settings.py
+++ SiteC/settings.py
@@ -95,6 +95,7 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
